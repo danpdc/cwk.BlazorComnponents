a4cea7333cdc2b520c96cd243122e3cdeb138644
Dan Patrascu-Baba <dan@danpatrascu.com>
2019-06-21 02:14:39 +0300

    Add basic chessboard component

---
+++ Chessboard/CWK.BlazorComponents.Chessboard/CwkChessboard.razor.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWK.BlazorComponents.Chessboard
+{
+    public class CwkChessboardModel : ComponentBase
+    {
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
+        [Parameter] protected string Width { get; set; }
+
+        private static string _fen;
+        [Parameter] protected string Fen
+        {
+            get { return _fen; }
+            set { _fen = value; }
+        }
+
+        private static string _orientation;
+        [Parameter] protected string Orientation
+        {
+            get { return _orientation; }
+            set { _orientation = value; }
+        }
+
+        private static EventCallback<string> _fenChanged;
+        [Parameter] protected EventCallback<string> FenHasChanged { get { return _fenChanged; } set { _fenChanged = value; } }
+
+        private static Stack<string> _previousFens = new Stack<string>();
+        private static Stack<string> _forwardFens = new Stack<string>();
+
+        public CwkChessboardModel()
+        {
+            Fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+            Orientation = "white";
+            
+        }
+
+        protected override async Task OnAfterRenderAsync()
+        {
+            await JsRuntime.InvokeAsync<object>("renderBoard", Fen, Orientation);
+
+        }
+
+        [JSInvokable]
+        public static void SetCurrentFen(string fen)
+        {
+            if (_forwardFens.Count > 0)
+                _forwardFens.Clear();
+            _previousFens.Push(_fen);
+            _fen = fen;
+            _fenChanged.InvokeAsync(_fen);
+
+        }
+
+        protected void Back()
+        {
+            if (_previousFens.Count > 0)
+            {
+                _forwardFens.Push(Fen);
+                Fen = _previousFens.Pop();
+                FenHasChanged.InvokeAsync(Fen);
+            }
+           
+        }
+
+        protected void Forward()
+        {
+            if (_forwardFens.Count > 0)
+            {
+                _previousFens.Push(Fen);
+                Fen = _forwardFens.Pop();
+                FenHasChanged.InvokeAsync(Fen);
+            }
+            
+        }
+    }
+}
---
+++ Chessboard/CWK.BlazorComponents.Chessboard/content/js/blazorChess.js
@@ -0,0 +1,58 @@
+﻿
+
+
+function renderBoard(position, orientation) {
+
+    var board,
+        game = new Chess(position);
+
+    function onDragStart(source, piece, position, orientation) {
+        // do not pick up pieces if the game is over
+        if (game.game_over()) return false;
+
+        // only pick up pieces for the side to move
+        if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+            (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+            return false;
+        }
+    }
+
+    function onDrop(source, target) {
+        // see if the move is legal
+        var move = game.move({
+            from: source,
+            to: target,
+            promotion: 'q' // NOTE: always promote to a queen for example simplicity
+        });
+
+        // illegal move
+        if (move === null) return 'snapback';
+
+        updateStatus();
+    }
+
+    // update the board position after the piece snap
+    // for castling, en passant, pawn promotion
+    function onSnapEnd() {
+        board.position(game.fen());
+        DotNet.invokeMethodAsync('CWK.BlazorComponents.Chessboard', 'SetCurrentFen', game.fen());
+    }
+
+    function updateStatus() {
+
+        document.getElementById("fen").value = game.fen();
+    }
+
+    var config = {
+        orientation: orientation,
+        draggable: true,
+        position: position,
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd
+    };
+    board = ChessBoard('board', config);
+
+    updateStatus();
+}
+
